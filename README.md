1edc2586ab375da5281bbdf062be05ac42d9bc3b
juniorro <juniorro98@outlook.com>
2017-04-01 13:08:26 -0400

    Configured README file

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # account-manager
 Online Bank Account Simulation
+
+/**
+* Author:  Roland Junior Toussaint
+* Version 1.0
+* Since   04/01/2017
+*/
+
+
+# INTRODUCTION
+------------
+
+This program simulates an online bank account service to deposit
+and withdraw money to either a savings or checking accounts,
+and to transfer money from one account to another.
+
+# REQUIREMENTS
+------------
+
+Java Compiler
+MySQL database
+Spring Framework
+
+
+# RECOMMENDED MODULES
+-------------------
+
+ None
+
+
+# INSTALLATION
+------------
+
+Java Virtual Machine
+Spring Framework
+MySQL Database
+
+
+# CONFIGURATION
+-------------
+
+None
+
+
+# TROUBLESHOOTING
+---------------
+
+A MySQL database named "accountmanager" need to be
+in place.
--- src/main/java/app/AccountManagerApplication.java
+++ src/main/java/app/AccountManagerApplication.java
@@ -31,45 +31,45 @@ public class AccountManagerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 
-// 		Client c1 = clientRepository.save(new Client("Morgan Freeman", "freeman@morgan.com"));
-// 		Client c2 = clientRepository.save(new Client("Jackson Samuel", "samuel@jackson.com"));
-// 		Client c3 = clientRepository.save(new Client("Denzel Washington", "denzel@washington.com"));
-// 		Client c4 = clientRepository.save(new Client("Kevin Hart", "hevin@hart.com"));
-// 		Client c5 = clientRepository.save(new Client("Micheal White", "micheal@white.com"));
+ 		Client c1 = clientRepository.save(new Client("Morgan Freeman", "freeman@morgan.com"));
+ 		Client c2 = clientRepository.save(new Client("Jackson Samuel", "samuel@jackson.com"));
+ 		Client c3 = clientRepository.save(new Client("Denzel Washington", "denzel@washington.com"));
+ 		Client c4 = clientRepository.save(new Client("Kevin Hart", "hevin@hart.com"));
+ 		Client c5 = clientRepository.save(new Client("Micheal White", "micheal@white.com"));
+
+//		//---------------------------------------------------
 //
-////		//---------------------------------------------------
-////
-//		 Account ac1 = accountRepository.save(new Checking("CA", new Date(), 1000, c1, 500));
-//		 Account ac2 = accountRepository.save(new Savings("SA", new Date(), 10000, c1, 2.5));
-////
-////		//---------------------------------------------------
-////
-//		 transactionRepository.save(new Deposit(new Date(), 2000.00, ac1));
-//		 transactionRepository.save(new Deposit(new Date(), 6000.00, ac1));
-////
-////
-////
-//		 transactionRepository.save(new Withdraw(new Date(), 400.00, ac1));
-//		 transactionRepository.save(new Withdraw(new Date(), 600.00, ac1));
-////
-////		//---------------------------------------------------
-////
-// 		transactionRepository.save(new Deposit(new Date(), 100.00, ac2));
-// 		transactionRepository.save(new Deposit(new Date(), 600.00, ac2));
-//// //
-// 		transactionRepository.save(new Withdraw(new Date(), 400.00, ac2));
-// 		transactionRepository.save(new Withdraw(new Date(), 600.00, ac2));
-////
-////
-////		//---------------------------------------------------
+		 Account ac1 = accountRepository.save(new Checking("CA", new Date(), 1000, c1, 500));
+		 Account ac2 = accountRepository.save(new Savings("SA", new Date(), 10000, c1, 2.5));
 //
-// 		daoService.makeDeposit("CA", 400.00);
-// 		daoService.makeDeposit("CA", 500.00);
-// 		daoService.makeDeposit("CA", 300.00);
-// 		daoService.makeDeposit("CA", 200.00);
-// 		daoService.makeDeposit("SA", 100.00);
-// 		daoService.makeDeposit("SA", 800.00);
-//		
+//		//---------------------------------------------------
+//
+		 transactionRepository.save(new Deposit(new Date(), 2000.00, ac1));
+		 transactionRepository.save(new Deposit(new Date(), 6000.00, ac1));
+//
+//
+//
+		 transactionRepository.save(new Withdraw(new Date(), 400.00, ac1));
+		 transactionRepository.save(new Withdraw(new Date(), 600.00, ac1));
+//
+//		//---------------------------------------------------
+//
+ 		transactionRepository.save(new Deposit(new Date(), 100.00, ac2));
+ 		transactionRepository.save(new Deposit(new Date(), 600.00, ac2));
+// //
+ 		transactionRepository.save(new Withdraw(new Date(), 400.00, ac2));
+ 		transactionRepository.save(new Withdraw(new Date(), 600.00, ac2));
+//
+//
+//		//---------------------------------------------------
+
+ 		daoService.makeDeposit("CA", 400.00);
+ 		daoService.makeDeposit("CA", 500.00);
+ 		daoService.makeDeposit("CA", 300.00);
+ 		daoService.makeDeposit("CA", 200.00);
+ 		daoService.makeDeposit("SA", 100.00);
+ 		daoService.makeDeposit("SA", 800.00);
+		
 
 	}
 
